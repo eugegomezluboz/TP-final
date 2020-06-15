4c03ff3663b86d47dffe730899712cc6674e6fce
fabian benitez <fabian2000benitez@gmail.com>
2020-06-15 16:55:54 -0300

    .

--- cEventos.h
+++ cEventos.h
@@ -1,8 +1,17 @@
 #pragma once
 
+struct fecha
+{
+	int hora, minutos, segundos;
+} 
+typedef fecha Fecha;
+
 class cEventos
 {
 private:
+	fecha hora_del_evento;
 public:
+	cEventos(fecha fech);
 
+	~cEventos() {}
 };
