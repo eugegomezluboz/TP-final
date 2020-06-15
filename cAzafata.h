275567ae6d8a55b51c30f4ec0178c744f02578dc
fabian benitez <fabian2000benitez@gmail.com>
2020-06-15 14:16:23 -0300

    agrego cAzafata y cMarshall. Todas las clases las estoy haciendo en base al UML que hiciste, pero no si lo actualizaste, igual cualquier cosa seria agregar lo nuevo

---
+++ cAzafata.h
@@ -0,0 +1,14 @@
+#pragma once
+
+class cAzafata
+{
+private:
+	bool disponible;
+public:
+	bool Ocupada();
+	bool EntregarComida();
+	bool EntregarBebida();
+	bool LlamadoCliente();
+	bool LlamadoPiloto();
+	bool Anuncios();
+};
---
+++ cMarshall.h
@@ -0,0 +1,10 @@
+#pragma once
+
+class cMarshall
+{
+private:
+
+public:
+	bool EncarcelarAlPasajeroArrebatado();
+};
+	
