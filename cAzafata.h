7df6c101573a8397a0e681e334d32e34a39e1ace
Foch Poch <gomezlubozeuge@gmail.com>
2020-06-17 15:09:58 -0300

    Agrego parametros

--- cAzafata.h
+++ cAzafata.h
@@ -6,9 +6,9 @@ private:
 	bool disponible;
 public:
 	bool Ocupada();
-	void EntregarComida();
-	void EntregarBebida();
-	void LlamadoCliente();
-	void LlamadoPiloto();
+	void EntregarComida(string pasajero);
+	void EntregarBebida(string pasajero);
+	void LlamadoCliente(string pasajero);
+	void LlamadoPiloto(string piloto);
 	void Anuncios();
 };
