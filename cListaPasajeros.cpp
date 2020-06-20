9018153a8635acf8fd7d093e2bc1693f76b5820c
fabian benitez <fabian2000benitez@gmail.com>
2020-06-19 23:57:03 -0300

    .

--- cListaPasajeros.cpp
+++ cListaPasajeros.cpp
@@ -8,3 +8,15 @@ cListaPasajeros::cListaPasajeros(int tm)
 	for (int i = 0; i < tm; i++)
 		pasajero[i] = NULL;
 }
+
+cListaPasajeros::~cListaPasajeros()
+{
+	if (pasajero != NULL) {
+		for (int i = 0; i < ca; i++)
+		{
+			if (pasajero[i] != NULL)
+				delete pasajero[i];
+		}
+		delete[] pasajero;
+	}
+}
