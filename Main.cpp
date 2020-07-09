34d7d47dc48d798121013f46df7d522ac901055d
fabian benitez <fabian2000benitez@gmail.com>
2020-07-09 18:45:04 -0300

    mas cosas

--- Main.cpp
+++ Main.cpp
@@ -3,11 +3,30 @@
 #include <string>
 #include <iostream>
 #include <stdlib.h>
+#include "cListaPasajeros.h"
+#include "cListaTripulacion.h"
 
 using namespace std;
 
 int main()
 {
+	cListaPasajeros list_1_pasajeros(200);
+
+	cPasajeros* pasajero1 = new cPasajeros("sfjiga","ejecutiva");
+
+	list_1_pasajeros.
+
+
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 	system("pause");
 	return 0;
 }
--- cPasajeros.h
+++ cPasajeros.h
@@ -1,13 +1,20 @@
 #pragma once
 #include "cPersona.h"
 
+struct tipo_pasajero
+{
+	string turista, primera_clase, ejecutiva;
+};
+tipo_pasajero Pasajero;
+
 class cPasajeros : public cPersona
 {
 public:
 	const string codigo;
 	const string clase;
+	tipo_pasajero tip_pas;
 private:
-	cPasajeros(const string cod,const string clas);
+	cPasajeros(const string cod, const string clas);
 	~cPasajeros() {};
 
 	void serLlevadoPorElAvion();
